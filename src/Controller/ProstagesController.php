d5ea1a81df7ba6f428aec6803cc428e3f6fe2cc6
Nathan Vacher <nathan.vacher33@gmail.com>
2022-01-05 10:16:11 +0100

    Mise en place du controleur métier pour voir les vues (TP1)

--- src/Controller/ProstagesController.php
+++ src/Controller/ProstagesController.php
@@ -9,12 +9,40 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProstagesController extends AbstractController
 {
     /**
-     * @Route("/prostages", name="prostages")
+     * @Route("/", name="prostages")
      */
     public function index(): Response
     {
         return $this->render('prostages/index.html.twig', [
             'controller_name' => 'ProstagesController',
+        ]);
+    }
+    /**
+     * @Route("/entreprises", name="pageEntreprises")
+     */
+    public function afficherPageEntreprises(): Response
+    {
+        return $this->render('prostages/affichageEntreprises.html.twig', [
+            'controller_name' => 'ProstagesController',
+        ]);
+    }
+    /**
+     * @Route("/formations", name="pageFormations")
+     */
+    public function afficherPageFormations(): Response
+    {
+        return $this->render('prostages/affichageFormations.html.twig', [
+            'controller_name' => 'ProstagesController',
+        ]);
+    }
+    /**
+     * @Route("/stages/{id}", name="pageStages")
+     */
+    public function afficherPageStages($id): Response
+    {
+        return $this->render('prostages/affichageStages.html.twig', [
+            'controller_name' => 'ProstagesController',
+            'id'=>$id,
         ]);
     }
 }
